e3bdbd3b808e373945c43b0cb579f9990c4562a9
Kai Battle <kblaz3@uab.edu>
2024-03-08 10:46:08 -0600

    filterbox + thumbnail frame

--- src/app/components/FilterBox.js
+++ src/app/components/FilterBox.js
@@ -1,18 +1,21 @@
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
+import {Forest} from '@mui/icons-material';
 export default function FilterBox (){
     const list = [
         {label: "Business"},
         {label: "Leisure"},
-        {label: "Exploration"},
+        {label: "Exploration", icon: Forest},
         {label: "Blended"},
         {label: "Winter"},
         {label: "Summer"}
     ]
     let options = list.map((option) => {
         const firstLetter = option.label[0].toUpperCase();
+        const icon = option.icon
         return {
           firstLetter: /[0-9]/.test(firstLetter) ? '0-9' : firstLetter,
+          icon,
           ...option,
         };
       });
@@ -23,7 +26,7 @@ export default function FilterBox (){
           options={options.sort((a, b) => -b.firstLetter.localeCompare(a.firstLetter))}
           groupBy={(option) => option.firstLetter}
           sx={{ width: 300 }}
-          renderInput={(params) => <TextField {...params} label="Movie" />}
+          renderInput={(params) => <TextField {...params} label="Filters" />}
         />
       );
 }
---
+++ src/app/components/Thumbnail.js
@@ -0,0 +1,7 @@
+
+
+export default function Thumbnail (){
+    return(
+        null
+    );
+}
