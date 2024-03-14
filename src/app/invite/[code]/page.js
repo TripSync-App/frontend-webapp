d1016acbda3781944c57a38f6fca8f14b4537896
Jayden <jpylesbusiness@gmail.com>
2024-03-14 16:32:34 -0500

    wip: add redeeming

--- src/app/invite/[code]/page.js
+++ src/app/invite/[code]/page.js
@@ -1,7 +1,30 @@
 "use client";
+import { API_URL } from "@/app/constants";
+import React, { useEffect } from "react";
 
 function InvitePage({ params }) {
   const inviteCode = params.code;
+  const token = localStorage.getItem("accessToken");
+
+  useEffect(() => {
+    fetch(`${API_URL}/teams/redeem-invite`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({ code: inviteCode }),
+    })
+      .then((response) => {
+        console.log(response.status);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        throw error;
+      });
+
+  }, [])
+
   return (
     <div>
       <p>Invite Code: {inviteCode}</p>
