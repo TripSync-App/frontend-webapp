5152c9134990973c7bdae546f47d7314ea2bf37d
Jayden Pyles <jpylesbusiness@gmail.com>
2024-04-08 17:34:12 -0500

    wip: discussion dialogs

--- src/app/discussions/[code]/Discussion.js
+++ src/app/discussions/[code]/Discussion.js
@@ -1,11 +1,18 @@
 import { Typography } from "@mui/material";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Discussion = ({ discussion }) => {
+  //TODO: load messages using useEffect
   return (
     <>
-      <div>
-        <Typography>{discussion.title}</Typography>
+      <div className="flex flex-row mr-auto ml-auto">
+        <div id="mainContent">
+          <Typography variant="h3">{discussion.title}</Typography>
+          <div id="messages">
+            <Typography variant="h4">Messages</Typography>
+          </div>
+        </div>
+        <div id="sideBar">Test</div>
       </div>
     </>
   );
