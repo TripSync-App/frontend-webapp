1ad7ba8e01753603db51d19a558fcff72716c500
Jayden Pyles <jpylesbusiness@gmail.com>
2024-04-08 19:10:28 -0500

    wip: send messages

--- src/app/discussions/[code]/Discussion.js
+++ src/app/discussions/[code]/Discussion.js
@@ -6,24 +6,50 @@ import Message from "./Message";
 
 const Discussion = ({ discussion }) => {
   let token = "";
+  let userData = {};
   try {
     token = localStorage.getItem("accessToken");
+    userData = JSON.parse(localStorage.getItem("userData"));
   } catch {}
+
   const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
 
   const getMessages = async () => {
     try {
-      await fetch(`/api/discussions/messages/${discussion.discussion_id}`, {
-        method: "GET",
+      const response = await fetch(
+        `/api/discussions/messages/${discussion.discussion_id}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+      const data = await response.json();
+      setMessages(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const createMessage = async (message) => {
+    const messageBody = {
+      author: userData.user_id,
+      text: message,
+      discussion: discussion.discussion_id,
+    };
+    try {
+      await fetch(`/api/messages`, {
+        method: "POST",
         headers: {
           "Content-Type": "application/json",
           Authorization: `Bearer ${token}`,
         },
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          setMessages(data);
-        });
+        body: JSON.stringify({ message: messageBody }),
+      });
+      getMessages();
     } catch (err) {
       console.log(err);
     }
@@ -31,7 +57,18 @@ const Discussion = ({ discussion }) => {
 
   useEffect(() => {
     getMessages();
-  }, []);
+  }, [discussion.discussion_id]);
+
+  const handleNewMessageChange = (event) => {
+    setNewMessage(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (!newMessage.trim()) return;
+    await createMessage(newMessage);
+    setNewMessage("");
+  };
 
   return (
     <>
@@ -41,6 +78,18 @@ const Discussion = ({ discussion }) => {
           <div className="p-4 h-[50%] w-full">
             <Map></Map>
           </div>
+          <form id="create-message" className="mt-4" onSubmit={handleSubmit}>
+            <input
+              type="text"
+              value={newMessage}
+              onChange={handleNewMessageChange}
+              placeholder="Type your message here..."
+              className="input"
+            />
+            <button type="submit" className="btn">
+              Send
+            </button>{" "}
+          </form>
           <div id="messages" className="mt-4">
             {messages.map((message, index) => (
               <Message message={message} key={index}></Message>
